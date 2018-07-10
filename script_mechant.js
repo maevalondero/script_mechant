2a9abda5568678d972004a8483d1ed17bdf6c61b
Maeva Londero <maeva.londero@camptocamp.com>
2018-07-10 11:14:28 +0200

    Bouton de telecharger fonctionnel

---
+++ script_mechant.js
@@ -0,0 +1,16 @@
+// Webcam.set({
+// 			width: 320,
+// 			height: 240,
+// 			image_format: 'jpeg',
+// 			jpeg_quality: 90
+// });
+// Webcam.attach( '#my_camera' );
+//
+// function prendre_photo() {
+// 			// prendre la photo
+// 			Webcam.snap( function(data_uri) {
+// 				// afficher
+// 				document.getElementById('results').innerHTML =
+// 					'<img src="'+data_uri+'"/>';
+// 			} );
+// 		}
